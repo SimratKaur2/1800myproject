104ca5fe293b8954eb8dd10f4e79ddfccdaf61f4
Simrat Kaur <simratkaur2@my.bcit.ca>
2022-12-20 15:55:13 -0800

    Modified the template.js and got started with the firebase

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -7,3 +7,15 @@ function loadSkeleton(){
     console.log($('#footerPlaceholder').load('./text/footer.html'));
 }
 loadSkeleton();  //invoke the function
+
+/*
+jQuery is a library with functions to make it easier to use JavaScript.
+$()- selects an element by "id" value
+
+jQuery Ajax(Asynchronous javascript and XML)
+
+a) is a library with functions to let us interact with files on the server.
+b)the load() is a powerful AJAX function to load(read) data from a text file
+that is sitting on the SERVER and puts the data into the selected element. 
+*/
+ 
